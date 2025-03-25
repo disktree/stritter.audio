054a7f28a457a8c33537d63c08506b5d6327669f
tong <tong@disktree.net>
2025-03-25 17:47:18 +0000

    Deploying to gh-pages from @ disktree/stritter.audio@045c7b50a93d868eabe6af49fa6020a86556b4ce 🚀

---
+++ js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+console.info('Source: https://github.com/disktree/stritter.audio');
+console.info('https://disktree.net');
+
+const stritter = {
+    url: "https://stritter.audio",
+    git: "https://github.com/disktree/stritter.audio.git",
+    // live: {
+    //     list: () => { return ['a','b','c']; }
+    // }
+};
+
+//window.addEventListener('load', _ => {
+    //const style = window.getComputedStyle(document.querySelector(':root'));
+    //THEME.bg = style.getPropertyValue('--bg');
+    //THEME.fg = style.getPropertyValue('--fg');
+//}, false);
+
